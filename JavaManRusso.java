e01f1452a31eab30c0d95cd9ee192fa6195c0027
ThatAnnoyingKid <57725498+ThatAnnoyingKid@users.noreply.github.com>
2022-10-02 22:00:12 -0700

    forgot to indent

--- JavaManRusso.java
+++ JavaManRusso.java
@@ -1,9 +1,32 @@
 class JavaManRusso {
   public static void main(String[] args) {
-    System.out.println("                   / \\\n                  /   \\\n                 /     \\\n                /       \\\n               / /// \\\\\\ \\\n              / __     __ \\"); //head
-    System.out.println("              |  @     @  |\n              |     $     |\n              |    <_>    |\n      ________|___________|________"); //face
-    System.out.println("     /                             \\\n    /     __     __    __    __     \\\n   /     |J |   |A |   |V|  |A |     \\\n  /      |__|   |__|   |_|  |__|      \\\n /                                     \\\n/_______@@@___________________@@@_______\\\n        @@@                   @@@\n        @@@                   @@@"); // body and legs, the legs begin with the @ signs
-    System.out.println("    ____|                       |____\n   |____|                       |____|\n     /\\                           /\\");
+    System.out.println(
+        "                   / \\\n"
+            + "                  /   \\\n"
+            + "                 /     \\\n"
+            + "                /       \\\n"
+            + "               / /// \\\\\\ \\\n"
+            + "              / __     __ \\"); // head
+    System.out.println(
+        "              |  @     @  |\n"
+            + "              |     $     |\n"
+            + "              |    <_>    |\n"
+            + "      ________|___________|________"); // face
+    System.out.println(
+        "     /                             \\\n"
+            + "    /     __     __    __    __     \\\n"
+            + "   /     |J |   |A |   |V|  |A |     \\\n"
+            + "  /      |__|   |__|   |_|  |__|      \\\n"
+            + " /                                     \\\n"
+            + "/_______@@@___________________@@@_______\\\n"
+            + "        @@@                  "
+            + " @@@\n"
+            + "        @@@                  "
+            + " @@@"); // body and legs, the legs begin with the @ signs
+    System.out.println(
+        "    ____|                       |____\n"
+            + "   |____|                       |____|\n"
+            + "     /\\                           /\\");
   }
 }
 /*I believe my grade for this assignment is 100 since all the requirements are satisficed*/
